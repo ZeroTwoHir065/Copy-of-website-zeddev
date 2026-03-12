12452d34dcbe68d67bafe2cbab7f0925c1c6cccd
ZeroTwoHir065 <dadadad5044@gmail.com>
2026-03-13 00:28:33 +0500

    add footer, main(half)

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const dropdowns = document.querySelectorAll('.header-dropdown-begin');
 
 dropdowns.forEach((dropdown) => {
-    const text = dropdown.textContent.replace('∨', '').replace('∧', '').trim();
-
     dropdown.addEventListener("mouseenter", () => {
-        dropdown.innerHTML = text + " &#8743;";
+        dropdown.textContent = dropdown.textContent.replace('∨', '∧');
     });
     dropdown.addEventListener("mouseleave", () => {
-        dropdown.innerHTML = text + " &#8744;";
+        dropdown.textContent = dropdown.textContent.replace('∧', '∨');
     });
 });
